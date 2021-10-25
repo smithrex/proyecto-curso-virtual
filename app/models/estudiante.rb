ff51f75610a1e45202bba963752af9c748fc4444
Rodrigo Herrera <90879379+smithrex@users.noreply.github.com>
2021-10-25 00:42:31 +0000

    incluyendo relaciones de curso

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -2,6 +2,10 @@ class Estudiante < ApplicationRecord
   
   belongs_to :curso
 
-  validates :nombre, :app_paterno, :app_materno,
-  presence: { message: "Este campo es requerido."}
+  validates :nombre, 
+    :app_paterno, 
+    :app_materno,
+    presence: { 
+    message: "Este campo es requerido."
+  }
 end
